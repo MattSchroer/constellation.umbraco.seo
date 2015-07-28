55dc8245fe68abc49663f4fd7ddc8757f1f10d4b
Matt Schroer <mschroer@genuineinteractive.com>
2015-07-28 13:49:32 -0400

    First commit. Began Umbraco refactoring.

---
+++ HttpHandlers/RobotsTxtHandler.cs
@@ -0,0 +1,61 @@
+﻿namespace Constellation.Sitecore.HttpHandlers
+{
+	using System.Text;
+	using System.Web;
+
+	/// <summary>
+	/// Handles requests for robots.txt and response with a reference to the hostname-specific
+	/// sitemap.xml file.
+	/// </summary>
+	public class RobotsTxtHandler : IHttpHandler
+	{
+		#region IHttpHandler Members
+		/// <summary>
+		/// Gets a value indicating whether this instance of the handler is reusable.
+		/// Required by IHttpHandler, not used by developers in this case.
+		/// </summary>
+		public bool IsReusable
+		{
+			get { return false; }
+		}
+
+		/// <summary>
+		/// Required by IHttpHandler, this is called by ASP.NET when an appropriate URL match is found.
+		/// </summary>
+		/// <param name="context">The current request context.</param>
+		public void ProcessRequest(HttpContext context)
+		{
+			var builder = new StringBuilder();
+
+			var rules = RobotsTxtHandlerConfiguration.Settings.Rules;
+
+			for (int i = 0; i < rules.Count; i++)
+			{
+				var rule = rules[i];
+				builder.AppendLine("UserAgent: " + rule.AgentName);
+
+				if (!rule.Allowed)
+				{
+					builder.AppendLine("Disallow: /");
+				}
+
+			}
+
+			builder.AppendLine("User-agent: *");
+
+			if (!RobotsTxtHandlerConfiguration.Settings.Allowed)
+			{
+				builder.AppendLine("Disallow: /");
+			}
+
+			builder.AppendLine();
+			builder.AppendLine("Sitemap: " + context.Request.Url.GetLeftPart(System.UriPartial.Authority) + "/sitemap.xml");
+
+			context.Response.Clear();
+			context.Response.ContentType = "text";
+			context.Response.Write(builder.ToString());
+			context.Response.End();
+		}
+		#endregion
+	}
+}
---
+++ HttpHandlers/SitemapXmlHandlerConfiguration.cs
@@ -0,0 +1,75 @@
+﻿namespace Constellation.Sitecore.HttpHandlers
+{
+	using System.Configuration;
+
+	/// <summary>
+	/// The sitemap xml handler configuration.
+	/// </summary>
+	public class SitemapXmlHandlerConfiguration : ConfigurationSection
+	{
+		/* <sitemapXmlHandler
+		 *		crawlerType="Constellation.Sitecore.HttpHandlers.SitemapXml.DefaultCrawler, Constellation.Sitecore.Seo"
+		 *		sitemapNodeType="Constellation.Sitecore.HttpHandlers.SitemapXml.DefaultSitemapNode, Constellation.Sitecore.Seo"
+		 *		cacheTimeoutMinutes="45" />
+		 */
+
+		/// <summary>
+		/// The settings.
+		/// </summary>
+		private static SitemapXmlHandlerConfiguration settings;
+
+		/// <summary>
+		/// Gets the settings.
+		/// </summary>
+		public static SitemapXmlHandlerConfiguration Settings
+		{
+			get
+			{
+				if (settings == null)
+				{
+					var config = ConfigurationManager.GetSection("sitemapXmlHandler");
+					settings = config as SitemapXmlHandlerConfiguration;
+				}
+
+				return settings;
+			}
+		}
+
+
+		/// <summary>
+		/// Gets the crawler type.
+		/// </summary>
+		[ConfigurationProperty("crawlerType", IsRequired = true)]
+		public string CrawlerType
+		{
+			get
+			{
+				return (string)base["crawlerType"];
+			}
+		}
+
+		/// <summary>
+		/// Gets the sitemap node type.
+		/// </summary>
+		[ConfigurationProperty("sitemapNodeType", IsRequired = true)]
+		public string SitemapNodeType
+		{
+			get
+			{
+				return (string)base["sitemapNodeType"];
+			}
+		}
+
+		/// <summary>
+		/// Gets the cache timeout minutes.
+		/// </summary>
+		[ConfigurationProperty("cacheTimeoutMinutes", DefaultValue = "45", IsRequired = true)]
+		public int CacheTimeoutMinutes
+		{
+			get
+			{
+				return (int)base["cacheTimeoutMinutes"];
+			}
+		}
+	}
+}
